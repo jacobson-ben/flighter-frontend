fc5a6d24d93618f1361641346aa460c0065a6d29
Ben Jacobson <jacobson.ben11@gmail.com>
2021-08-05 10:17:23 -0400

    added flight times to cards

--- src/flights/FlightCard.js
+++ src/flights/FlightCard.js
@@ -1,9 +1,19 @@
 import './FlightCard'
 
 function FlightCard({flightData}) {
-  console.log(flightData)
+  console.log(flightData);
+  const departDate = new Date(flightData.itineraries[0].segments[0].departure.at);
+  const departTime = departDate.toLocaleTimeString().replace(':00 ', '')
+
+  const arrivalDate = new Date(flightData.itineraries[0].segments[flightData.itineraries[0].segments.length - 1].arrival.at);
+  const arrivalTime = arrivalDate.toLocaleTimeString().replace(':00 ', '')
+  
   return (
-    <div className="card"> FlightCard </div>
+    <div className="card"> FlightCard 
+    <div className="card-body">
+      <h6 className="card-title">{departTime} - {arrivalTime}</h6>
+    </div>
+    </div>
   )
 }
 
--- src/flights/FlightResults.js
+++ src/flights/FlightResults.js
@@ -1,17 +1,12 @@
 import FlightCard from './FlightCard';
 
 function FlightResults({flights}) {
-  /*
-        {
-        flights.length > 1 &&
-        flights.map(flight => <FlightCard flightData={flight}/>)
-  } */
 
   return (
     <div>
       {
         flights.length > 0 &&
-        flights.map(flight => <FlightCard flightData={flight}/>)
+        flights.map(flight => <FlightCard flightData={flight} key={flight.id}/>)
       }
     </div>
   )
